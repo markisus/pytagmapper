d553eeb0e34d5b64fc08e5989354f4c3ae57ec43
Mark Liu <markisus@gmail.com>
2021-12-01 12:50:54 -0500

    generalize project() for more than 4 keypoints

--- pytagmapper/project.py
+++ pytagmapper/project.py
@@ -20,44 +20,48 @@ def get_corners_mat2d(size):
     mat[:2,:] *= size
     return mat
 
-def project(camera_matrix, tx_camera_tag, corners_mat):
-    camera_corners = tx_camera_tag @ corners_mat
-    dimage_corners_dcamera = np.empty((8,6))
+def project(camera_matrix, tx_camera_tag, keypoints_mat):
+    # keypoints_mat is expected to be (2, n)
+    # where n is the number of keypoints
+    num_kps = keypoints_mat.shape[1]
+    
+    camera_kps = tx_camera_tag @ keypoints_mat
+    dimage_kps_dcamera = np.empty((num_kps*2,6))
 
-    image_corners = camera_matrix @ camera_corners[:3,:]
-    # print("before homog\n", image_corners)
-    # print("after homog\n", image_corners)
+    image_kps = camera_matrix @ camera_kps[:3,:]
+    # print("before homog\n", image_kps)
+    # print("after homog\n", image_kps)
 
     for i in range(6):
         pert = np.zeros((6,1))
         pert[i,0] = 1
         pertmat = se3_to_matrix(pert)
         # print("pertmat", i, "is\n", pertmat)
-        dimage_corners = -camera_matrix @ (pertmat @ camera_corners)[:3,:] # 3 x 4
-        for corner_idx in range(4):
-            x = image_corners[0, corner_idx]
-            y = image_corners[1, corner_idx]
-            z = image_corners[2, corner_idx]
+        dimage_kps = -camera_matrix @ (pertmat @ camera_kps)[:3,:] # 3 x num_kps
+        for kp_idx in range(num_kps):
+            x = image_kps[0, kp_idx]
+            y = image_kps[1, kp_idx]
+            z = image_kps[2, kp_idx]
 
-            dx = dimage_corners[0, corner_idx]
-            dy = dimage_corners[1, corner_idx]
-            dz = dimage_corners[2, corner_idx]
+            dx = dimage_kps[0, kp_idx]
+            dy = dimage_kps[1, kp_idx]
+            dz = dimage_kps[2, kp_idx]
 
-            # print("corner", corner_idx, "dx, dy, dz", dx, dy, dz)
+            # print("corner", kp_idx, "dx, dy, dz", dx, dy, dz)
             # dxh = dx
             # dyh = dy
             dxh = (1/z)*dx + (-x/z**2)*dz
             dyh = (1/z)*dy + (-y/z**2)*dz
-            dimage_corners_dcamera[  2*corner_idx, i] = dxh
-            dimage_corners_dcamera[2*corner_idx+1, i] = dyh
+            dimage_kps_dcamera[  2*kp_idx, i] = dxh
+            dimage_kps_dcamera[2*kp_idx+1, i] = dyh
             
-    dimage_corners_dtag = dimage_corners_dcamera @ -SE3_adj(tx_camera_tag)
+    dimage_kps_dtag = dimage_kps_dcamera @ -SE3_adj(tx_camera_tag)
 
-    for i in range(4):
-        image_corners[:,i] /= image_corners[2,i] # homogenize
-    image_corners = np.reshape(image_corners[:2,:], (8,1), 'F')
+    for i in range(num_kps):
+        image_kps[:,i] /= image_kps[2,i] # homogenize
+    image_kps = np.reshape(image_kps[:2,:], (num_kps*2,1), 'F')
 
-    return image_corners, dimage_corners_dcamera, dimage_corners_dtag
+    return image_kps, dimage_kps_dcamera, dimage_kps_dtag
 
 if __name__ == "__main__":
     rng = np.random.default_rng(0)
