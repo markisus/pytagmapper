2792eb66dd8a3d360cbbbc531f615c499f22391f
Mark Liu <markisus@gmail.com>
2023-03-14 14:54:38 -0400

    Add geometry functions

--- pytagmapper/geometry.py
+++ pytagmapper/geometry.py
@@ -95,6 +95,9 @@ def so3_to_matrix(so3):
     ])
     return so3_matrix
 
+def so3_to_vector(so3):
+    return np.array([[so3[2,1], so3[0,2], so3[1,0]]]).T
+
 def se3_to_matrix(se3):
     result = np.empty((4,4))
     result[:3,:3] = so3_to_matrix(se3[:3,:])
@@ -144,8 +147,8 @@ def se2_exp(se2):
     r_p = (x + 1j*y)/theta
     r = r_p * 1j
     rotation = np.cos(theta) + 1j*np.sin(theta)
-    ep = r + (-r * rotation);
-    ed = rotation;
+    ep = r + (-r * rotation)
+    ed = rotation
 
     return np.array([
         [ed.real, -ed.imag, ep.real],
@@ -180,6 +183,146 @@ def SE3_inv(SE3):
     result[3,:] = [0, 0, 0, 1]
     return result
 
+def almost_equals(a, b):
+    return abs(a - b) < 1e-9
+
+def SO3_log_decomposed(SO3):
+    """
+    Returns so3_hat, the normalized version of the so3
+    vector corresponding to the input SO3, and theta, the
+    magnitude.
+    """
+    trace = np.trace(SO3)
+    is_identity = almost_equals(trace, 3)
+
+    # Edge case: identity
+    if is_identity:
+        theta = 0
+        omega_hat = np.array([[1, 0, 0]], dtype=float).T
+        return omega_hat, theta
+
+    # Edge case: rotation of k*PI
+    if almost_equals(trace, -1):
+        theta = np.pi
+        r33 = SO3[2,2]
+        r22 = SO3[1,1]
+        r11 = SO3[0,0]
+
+        if not almost_equals(1.0 + r33, 0):
+            omega_hat = np.array([[SO3[0,2], SO3[1,2], 1+SO3[2,2]]]).T
+            omega_hat /= (2 * (1 + r33))**0.5
+            return omega_hat, theta
+
+        if not almost_equals(1.0 + r22, 0):
+            omega_hat = np.array([[SO3[0,1], 1+SO3[1,1], SO3[2,1]]]).T
+            omega_hat /= (2 * (1 + r22))**0.5
+            return omega_hat, theta
+
+        omega_hat = np.array([[1+SO3[0,0], SO3[1,0], SO3[2,0]]]).T
+        omega_hat /= (2 * (1 + r11))
+        return omega_hat, theta
+
+    # normal case
+    # htmo means Half of Trace Minus One
+    htmo = 0.5 * (trace - 1)
+    theta = np.arccos(htmo)
+    sin_acos_htmo = (1.0 - htmo*htmo)**0.5
+    omega_mat = 0.5/sin_acos_htmo * (SO3 - SO3.T)
+    omega_hat = so3_to_vector(omega_mat)
+    return omega_hat, theta
+
+def SO3_log(SO3):
+    so3_hat, theta = SO3_log_decomposed(SO3)
+    return so3_hat * theta
+
+def barfoot_Q(se3):
+    rho = so3_to_matrix(se3[3:,:])
+    theta = so3_to_matrix(se3[:3,:])
+    rho_theta = (rho@theta)
+    rho_theta2 = (rho_theta@theta)
+    theta_rho_theta = (theta@rho_theta)
+    theta_rho = (theta@rho)
+    theta2_rho = (theta@theta_rho)
+
+    angle = np.linalg.norm(se3[:3,:])
+    if (angle < 1e-6):
+        angle2 = angle*angle
+        angle4 = angle2*angle2
+        c1 = 1.0/6 - angle2/120 + angle4/5040
+        c2 = 1.0/24 - angle2/720 + angle4/40320
+        c3 = 1.0/120 - angle2/2520 + angle4/120960
+    else:
+        angle2 = angle*angle
+        angle3 = angle2*angle
+        angle4 = angle2*angle2
+        angle5 = angle3*angle2
+        sn = np.sin(angle)
+        cs = np.cos(angle)
+        c1 = (angle - sn)/angle3
+        c2 = -(1.0 - angle2/2 - cs)/angle4
+        c3 = -0.5*(
+            (1.0 - angle2/2 - cs)/angle4 -
+            3.0*(angle - sn - angle3/6)/angle5)
+
+    line1 = 0.5*rho + c1*(theta_rho + rho_theta + theta_rho_theta)
+    line2 = c2*(theta2_rho + rho_theta2 - 3.0*theta_rho_theta)
+    line3 = c3*(theta_rho_theta@theta + theta@theta_rho_theta)
+    return line1 + line2 + line3
+
+def SO3_left_jacobian(so3):
+    theta = np.linalg.norm(so3)
+    theta2 = theta*theta
+    theta3 = theta2*theta
+    theta4 = theta2*theta2
+
+    theta_mat = so3_to_matrix(so3)    
+    if (theta < 1e-6):
+        coeff1 = 0.5 - theta2/24 + theta4/720
+        coeff2 = 1.0/6 - theta2/120 + theta4/5040
+    else:
+        cs = np.cos(theta)
+        sn = np.sin(theta)
+        coeff1 = (1.0 - cs)/theta2
+        coeff2 = (theta - sn)/theta3
+
+    result = np.eye(3, dtype=float) + coeff1*theta_mat + coeff2*theta_mat@theta_mat
+    return result
+
+def se3_left_jacobian(se3):
+    SO3_lj = SO3_left_jacobian(se3[:3,:])
+    Q = barfoot_Q(se3)
+    result = np.empty((6,6), dtype=float)
+    result[:3,:3] = SO3_lj
+    result[3:,3:] = SO3_lj
+    result[:3,3:] = 0
+    result[3:,:3] = Q
+    return result
+
+def x_cotx(x):
+    c2 = -1.0 / 3
+    c4 = -1.0 / 45
+    c6 = -2.0 / 945
+    c8 = -1.0 / 4725
+    c10 = -2.0 / 93555
+    x2 = x * x
+    x4 = x2 * x2
+    x6 = x4 * x2
+    x8 = x4 * x4
+    x10 = x8 * x2
+    return 1.0 + c2 * x2 + c4 * x4 + c6 * x6 + c8 * x8 + c10 * x10
+
+def SE3_log(SE3):
+    omega_hat, theta = SO3_log_decomposed(SE3[:3,:3])
+    omega_vec = omega_hat * theta
+    omega = so3_to_matrix(omega_vec)
+    p = SE3[:3,3:]
+    omega_p = omega @ p
+    v_theta = p - 0.5*theta*omega_p + (1.0 - x_cotx(theta/2)) * omega @ omega_p
+    result = np.empty((6,1), float)
+    result[:3,:] = omega_vec
+    result[3:,:] = v_theta
+    return result
+    
 def fix_SE3(SE3):
     Rx = SE3[:3, 0]
     Ry = SE3[:3, 1]
@@ -305,3 +448,49 @@ if __name__ == "__main__":
 
     fix_SE3(SE3)
     print("Fixed SE3\n", SE3)
+
+    
+    so3 = np.array([[0.2, -0.8, 0.1]]).T
+    so3_matrix = so3_to_matrix(so3)
+    SO3 = scipy.linalg.expm(so3_matrix)
+    print("SO3 matrix")
+    print(SO3)
+    so3_hat, theta = SO3_log_decomposed(SO3)
+    so3_result = so3_hat * theta
+    print(so3_result, so3)
+    print("SO3_result matrix")
+    SO3_result = scipy.linalg.expm(so3_to_matrix(so3_result))
+    print(SO3_result)
+
+    se3 = rng.random((6,1))
+    SE3 = se3_exp(se3)
+    se3_result = SE3_log(SE3)
+    print("se3, se3_result", se3.T, se3_result.T)
+
+    logM = se3
+    M = SE3
+    # exp(Jl_logM [Δ]) * M ~= exp(logM + [Δ])
+    # exp(Jl_logM [Δ]) ~= exp(logM + [Δ]) M_inv
+    # Jl_logM [Δ] ~= log(exp(logM + [Δ]) M_inv)
+    # Jl_logM [Δ] ~= lim_t->0 [log(exp(logM + [Δ t]) M_inv) / t]
+
+    for i in range(6):
+        print("i == ", i)
+        perturb = np.zeros((6,1), dtype=float)
+        perturb[i,0] = 1
+        eps = 1e-3
+        expected = SE3_log(se3_exp(logM + perturb*eps) @ SE3_inv(M))/eps
+        print("Expected ", expected.T)
+        print("Actual ", (se3_left_jacobian(logM) @ perturb).T)
+
+
+    # dxy_dobject = dxy_campoint * dcampoint_dobject
+    # dobject is an se3 perturbation
+    
+    # dxy = dxy_dobject * dse3
+    #     = dxy_dobject * log(tx_world_object2 * tx_world_object1.inv)
+    #     = dxy_dobject * log(exp(se3_world_object1 + delta) * tx_world_object1.inv)
+    #     = dxy_dobject * jl(se3_world_object1) * delta
+
+    
+    
